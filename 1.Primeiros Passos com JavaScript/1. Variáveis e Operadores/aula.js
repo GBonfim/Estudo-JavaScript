76ab0d00bb12a3a8b41aec56cc53c64491b4120b
GBonfim <gustavobonfim1199@gmail.com>
2022-10-27 19:03:54 -0300

    primeiro módulo, primeiro capítulo

---
+++ 1.Primeiros Passos com JavaScript/1. Variáveis e Operadores/aula.js
@@ -0,0 +1,8 @@
+
+
+let variavel = 10; //varia
+const pi = 3.14; //NÃO varia, seu valor não pode ser modificado
+
+variavel += 20;
+
+console.log(variavel);
---
+++ 1.Primeiros Passos com JavaScript/1. Variáveis e Operadores/primeiroDesafio.js
@@ -0,0 +1,18 @@
+/*
+Faça um programa para calcular o valor gasto de combustível em uma viagem
+Terá três variáveis, sendo elas: 
+1 - Preço do combustível;
+2 - Gasto médio de combustível do carro por Km;
+3 - Distância em Km da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem
+*/
+
+const precoCombustivel = 5.79;
+const kmPorLitro = 12; //12km por litro
+const distancia = 1580; //km
+
+const litrosConsumidos = distancia / kmPorLitro;
+const valorGasto = litrosConsumidos * precoCombustivel;
+
+console.log("O valor gasto para realizar a viagem será de R$" + valorGasto.toFixed(2));
