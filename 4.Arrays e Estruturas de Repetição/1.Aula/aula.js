7e8f0424b1d9f4591ab4bda709008239eefac134
GBonfim <gustavobonfim1199@gmail.com>
2022-11-12 05:48:29 -0300

    quarto módulo (arrays e estruturas de repetição) AULA

---
+++ 4.Arrays e Estruturas de Repetição/1.Aula/aula.js
@@ -0,0 +1,29 @@
+/*
+const alunos = ['João', 'Vitor', 'Marina'];
+
+alunos.push('Renan');
+
+alunos[4] = 'Vinicius';
+
+alunos.push(777);
+
+console.log(alunos);
+
+console.log(alunos.pop()); //desloca o último elemento
+
+console.log(alunos.shift()); //desloca o primeiro elemento
+
+console.log(alunos);    
+*/
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+const somaNotas = notas[0] + notas[1] + notas[2] + notas[3] + notas [4];
+
+console.log(somaNotas / notas.length);
---
+++ 4.Arrays e Estruturas de Repetição/1.Aula/aula2.js
@@ -0,0 +1,6 @@
+const notas = [];
+
+for (let i = 0; i < 5; i++) {
+    notas.push(i);
+    console.log(notas);
+}
